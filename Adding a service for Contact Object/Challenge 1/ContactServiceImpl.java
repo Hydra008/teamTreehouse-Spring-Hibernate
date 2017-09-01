b5675aa51d708ceeac0cad5b845346f608ecff92
Hydra008 <kshah2@lakeheadu.ca>
2017-09-01 16:50:04 -0400

    Create ContactServiceImpl.java

---
+++ Adding a service for Contact Object/Challenge 1/ContactServiceImpl.java
@@ -0,0 +1,15 @@
+package com.teamtreehouse.contactmgr.service;
+
+import com.teamtreehouse.contactmgr.dao.ContactDao;
+import com.teamtreehouse.contactmgr.model.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class ContactServiceImpl implements ContactService
+{
+ @Override
+ public Contact findById(Long id)
+ {
+ return null;
+ }
+}
