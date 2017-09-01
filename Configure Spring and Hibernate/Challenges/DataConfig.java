e9cad3b9d9ade4a7cb756afbd69d895b25c710d3
Hydra008 <kshah2@lakeheadu.ca>
2017-09-01 12:17:47 -0400

    Create DataConfig.java

---
+++ Configure Spring and Hibernate/Challenges/DataConfig.java
@@ -0,0 +1,17 @@
+package com.teamtreehouse.contactmgr.config;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+@Configuration
+public class DataConfig {
+
+}
